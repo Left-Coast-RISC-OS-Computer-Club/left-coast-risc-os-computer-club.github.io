0d29c47a7f448aff448f91fb0ae7cadad2603759
R. S. Doiel <rsdoiel@gmail.com>
2025-08-29 12:40:33 -0700

    commiting to

---
+++ website.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+for FNAME in contributing_news_items.md index.md README.md riscos.md; do
+  HTML_NAME="$(basename "${FNAME}" ".md").html"
+  printf "%s -> %s\n" "${FNAME}" "${HTML_NAME}" 
+  pandoc -s -f markdown -t html5 "${FNAME}" >"${HTML_NAME}"
+  git add "${HTML_NAME}"
+done
+
+antenna harvest
+antenna generate
+
+# Build our search indexes now that we have HTML pages.
+pagefind \
+  --verbose \
+  --force-language en \
+  --site .
+if [ -d .git ] ; then git add pagefind; fi
